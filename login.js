dd25aa9c14d9e15cb2cc82f04646a4b6bf8f1cd0
M4isv <boladaomailson@gmail.com>
2022-07-13 01:16:16 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,35 @@
+/*document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, {fullWidth: true,
+indicators: true,});
+  });
+*/
+
+document.addEventListener('DOMContentLoaded', () => {
+  // initialize carousel
+  const carousel = document.querySelectorAll('.carousel');
+  M.Carousel.init(carousel, {
+    fullWidth: true,
+    indicators: true, // this option is require for autoplay functionnality
+  });
+  
+  // custom function for autoplaying 
+  let indicatorItems = document.querySelectorAll('.carousel .indicator-item'),
+      slideTime = 3000,
+      activeClass = "active";
+  
+
+  setInterval(() => {
+    indicatorItems.forEach(el => {
+      if (el.classList.contains(activeClass)) {
+        sib = el.nextElementSibling;
+        if (sib == null) {
+          indicatorItems[0].click();
+        } else {
+          sib.click()
+        }
+      }
+    });
+  }, slideTime);
+});
+
